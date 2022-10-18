19c027bae3ed4a08639e122531fa8ab5d962dd2d
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2022-10-18 05:30:24 -0700

    Create Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.js

---
+++ Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.js
@@ -0,0 +1,15 @@
+var numOfSubarrays = function(arr, k, threshold) {
+    
+    let count = 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        if (i + 1 < k) continue;
+        if (sum / k >= threshold) {
+            count += 1;
+        };
+        sum -= arr[1 + i - k];
+    };
+    return count;
+    
+};
