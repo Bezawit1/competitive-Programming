60200b3dd182ce326bf3d03ad5bdd34d9343c2aa
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2022-10-22 04:13:27 -0700

    Create Sort Colors.js

---
+++ Sort Colors.js
@@ -0,0 +1,14 @@
+var sortColors = function(nums) {
+  for(let i = 0 ; i<nums.length ; i++){
+    for(let j =0 ; j<nums.length ;j++)
+   if(nums[j]>nums[j+1]){
+  var temp=nums[j];
+  nums[j]=nums[j+1];
+  nums[j+1]=temp;
+  }
+  
+  
+  }
+      return  nums;
+    
+};
