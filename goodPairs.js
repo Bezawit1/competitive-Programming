5c68017e2905934b774e6be5e50a301eb22b2fff
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2022-11-13 02:16:33 -0800

    Update goodPairs.js

--- goodPairs.js
+++ goodPairs.js
@@ -1,12 +1,12 @@
 var numIdenticalPairs = function(nums) {
-    var count = 0 ;
-    for(let i = 0 ; i<nums.length ; i++){
-for(let j = 0 ; j<nums.length ; j++){
-if(nums[i]==nums[j]&&i<j){
-count++;
-}
-}
-
-}
-    return count;
+   nums.sort((a,b)=>a-b);
+let count=0;
+for(let i = 0 ; i<nums.length-1;i++){
+  for(let j = 1;j<nums.length;j++){
+    if(nums[i]===nums[j]&&i<j)
+    count++
+  }
+    
+  }
+  return count;
 };
