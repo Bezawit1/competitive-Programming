8ba58f7c900e456810da5bd01bb334a2b6700bea
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-06-28 02:52:16 -0700

    Create PythonLists.py

---
+++ PythonLists.py
@@ -0,0 +1,25 @@
+ N = int(input())
+    lst=[]
+    
+    
+    for _ in range(N):
+       
+        cmd, *args = input().split()
+        
+        args = list(map(int, args))
+       
+        if cmd == "insert":
+            lst.insert(args[0], args[1])
+        elif cmd == "print":
+            print(lst)
+        elif cmd == "remove":
+            lst.remove(args[0])
+        elif cmd == "append":
+            lst.append(args[0])
+        elif cmd == "sort":
+            lst.sort()
+        elif cmd == "pop":
+            lst.pop()
+        elif cmd == "reverse":
+            lst.reverse()
+        
