628fb749b0c5a1482583e81f7080db00799a2b18
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-07-06 03:18:17 -0700

    Create Reverse Linked  List.py

---
+++ Reverse Linked  List.py
@@ -0,0 +1,10 @@
+def reverseList(self, head):
+        prev=None
+        current=head
+        while(current is not None):
+            next=current.next
+            current.next=prev
+            prev=current
+            current=next
+        head=prev
+        return head
