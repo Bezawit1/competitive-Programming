800478ac096c0a914983fd3ddf2eed32346c0172
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-07-12 03:33:47 -0700

    Create Rotate List.py

---
+++ Rotate List.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def rotateRight(self, head, k):
+        if not head or not head.next or k == 0:
+             return head
+        
+        length = 1
+        last = head
+        while last.next:
+            length += 1
+            last = last.next
+
+        
+        k = k % length
+        if k == 0:
+            return head
+
+        
+        new_head_pos = length - k
+        curr = head
+        for _ in range(new_head_pos - 1):
+            curr = curr.next
+
+        
+        new_head = curr.next
+        curr.next = None
+        last.next = head
+
+        return new_head
