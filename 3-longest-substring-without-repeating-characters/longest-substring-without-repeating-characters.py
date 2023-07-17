41169a3619b6eb5d7fc0a8b7f8578eea1d4f85e5
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-07-17 03:11:47 -0700

    Time: 45 ms (64.96%) | Memory: 14.1 MB (47.81%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,18 +3,17 @@ class Solution(object):
         hashmap={}
         maxLen=0
         left=0
-        if s == ' ': 
-            return 1
+       
         
         for i in range(len(s)):
             char=s[i]
             if char in hashmap and hashmap[char]>=left :
-                left = hashmap[char] + 1
-                
-            
+                left = hashmap[char] + 1  
+                  
             hashmap[char]=i
             curr_len = i-left+1
             maxLen=max(maxLen,curr_len)
+
         return maxLen 
 
 
