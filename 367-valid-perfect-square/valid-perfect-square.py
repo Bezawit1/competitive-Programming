8f6c611604d6c20675d0f57755a59b84eaa1265a
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-07-21 09:17:34 -0700

    Time: 3 ms (100.00%) | Memory: 13.2 MB (62.31%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        left , right = 0 , num
+        while left <= right:
+            mid = left + (right - left)//2
+            square = mid * mid
+            if square == num:
+                return True
+            elif square > num:
+                right = mid -1
+            else:
+                left = mid +1
+        return False
+       
