cff14504d0a70e9193a63396696a1b5d5891e8e7
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-08-16 16:44:13 +0300

    Time: 583 ms (60.84%) | Memory: 18.3 MB (83.22%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        trust_people= set()
+        count ={} 
+        for i,j in trust:
+            trust_people.add(i)
+            if j in count:
+                count[j] +=1
+            else:
+                count[j] =1
+        for i in range(1,n+1):
+            if i not in trust_people and count.get(i,0) == n-1:
+                return i
+        return -1
+       
+            
+
+        
