68e3aefe82fedb4e4de2381b43db799255078b27
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-08-24 17:01:45 +0300

    Time: 572 ms (57.02%) | Memory: 18.3 MB (92.87%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def findJudge(self, n, trust):
-        trust_people= []
-        count={} 
-        for i,j in trust:
-            trust_people.append(i)
-            if j in count:
-                count[j] +=1
-            else:
-                count[j] =1
-        for i in range(1,n+1):
-            if i not in trust_people and count.get(i,0) == n-1:
+        trust_people=[0]*(n + 1)
+        trust_count = [0] * (n + 1)
+
+
+        for i ,j in trust:
+            trust_people[i] += 1
+            trust_count[j] += 1
+        for i in range(1,n + 1):
+            if trust_people[i] ==0 and trust_count[i] == n-1:
                 return i
         return -1
-       
-       
+        
+
+
