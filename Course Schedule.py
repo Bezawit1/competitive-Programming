0e5815eb3a870bbef0f4f83dc437faa6171159f1
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-09-18 22:24:05 -0700

    Create Course Schedule.py

---
+++ Course Schedule.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[course].append(prereq)
+        
+    
+        visited = [0] * numCourses
+        
+    
+        def dfs(course):
+            if visited[course] == -1:
+                return False  
+            if visited[course] == 1:
+                return True   
+            
+            visited[course] = -1  
+            for prereq in graph[course]:
+                if not dfs(prereq):
+                    return False
+            
+            visited[course] = 1  
+            return True
+        
+    
+        for i in range(numCourses):
+            if not dfs(i):
+                return False 
+        
+        return True 
+            
+        
