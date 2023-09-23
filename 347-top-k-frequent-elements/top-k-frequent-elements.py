09145ec5e896e74d51d780fc9ad280f4cf28235e
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-09-23 00:29:58 -0700

    Time: 67 ms (90.36%) | Memory: 16.1 MB (99.97%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        hashmap={}
+        hashmap = {}
         res=[]
         for i in nums:
-            hashmap[i]=hashmap.get(i,0) +1
-        sorted_hashmap = sorted(hashmap.items() , key = lambda x:x[1] , reverse=True)
-        for i in range(k):
-            res.append(sorted_hashmap[i][0])
-        return res
+            hashmap[i] = hashmap.get(i,0) +1
+        
+        return [item[0] for item in sorted(hashmap.items(), key=lambda x: x[1], reverse=True)[:k]]
+
+
+        
+        
