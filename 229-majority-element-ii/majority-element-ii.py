5b71c52d669fcb9ad87e1b5f30b1dd0ffef89e90
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-10-05 05:44:36 -0700

    Time: 90 ms (23.80%) | Memory: 14.4 MB (51.07%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        freq = len(nums)//3
+        counts = Counter(nums)
+        res = []
+
+        for num,count in counts.items():
+            if count>freq:
+                res.append(num)
+        return res
+
+        
