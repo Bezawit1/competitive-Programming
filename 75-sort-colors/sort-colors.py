6c4e652df418a9de3c3a91138388c0875e5e0195
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-10-07 07:11:08 -0700

    Time: 14 ms (79.34%) | Memory: 13.1 MB (75.60%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,12 +1,14 @@
 class Solution(object):
     def sortColors(self, nums):
-        counters = [0]*3
+
+        countingArray = [0]*3
         for num in nums:
-            counters[num]+=1
-        idx = 0
-        for color in range(3):  # Iterate over the colors: 0, 1, and 2
-            for _ in range(counters[color]):
-                nums[idx] = color
-                idx += 1
+            countingArray[num]+=1
+        index = 0
+        for color in range(3): 
+            for _ in range(countingArray[color]):
+                nums[index] = color
+                index += 1
         return nums
+       
         
