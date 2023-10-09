f43cbaddc0f8937f703310f0973a1071dc260b27
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-10-09 01:06:39 -0700

    Time: 968 ms (30.42%) | Memory: 35.5 MB (15.21%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def maximumGap(self ,nums):
+        if len(nums) < 2:
+            return 0
+
+        min_num, max_num = min(nums), max(nums)
+        bucket = max(1, (max_num - min_num) // (len(nums) - 1))
+        num_buckets = (max_num - min_num) // bucket + 1
+        buckets = [[] for _ in range(num_buckets)]
+
+        for num in nums:
+            index = (num - min_num) // bucket
+            buckets[index].append(num)
+
+        prev_max = min_num
+        max_gap = 0
+
+        for i in range(num_buckets):
+            if buckets[i]:
+                min_in_bucket, max_in_bucket = min(buckets[i]), max(buckets[i])
+                max_gap = max(max_gap, min_in_bucket - prev_max)
+                prev_max = max_in_bucket
+
+        return max_gap
+
+
+        
