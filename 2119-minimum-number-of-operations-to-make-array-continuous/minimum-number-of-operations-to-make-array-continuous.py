26106752bf77d4a16bcae52426677abda8ff23d0
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-10-09 23:41:16 -0700

    Time: 630 ms (40.00%) | Memory: 31.1 MB (30.00%) - LeetSync

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minOperations(self,nums):
+      # sort the array and put it in a set to avoid duplicates
+        unique_nums = sorted(set(nums))
+        n = len(nums)
+        i = 0
+        j = 0
+        min_ops = n
+        for  i in range(len(unique_nums)):
+            while j < len(unique_nums) and unique_nums[j] - unique_nums[i] <=len(nums) - 1:
+                j+=1
+                min_ops = min(min_ops , n -(j - i))
+        return min_ops
+    
+            
+            
+
+
+        
+
+
+
+
+
+       
