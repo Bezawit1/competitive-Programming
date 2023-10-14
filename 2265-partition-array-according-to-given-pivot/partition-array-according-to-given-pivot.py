e48effb919c6bb50607055528a848d86ef2b283b
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-10-14 00:59:16 -0700

    Time: 1057 ms (60.00%) | Memory: 31.7 MB (24.00%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -3,17 +3,14 @@ class Solution(object):
         lower_bound = []
         upper_bound = []
         equal = []
-        i = 0 
-       
-        while i  < len(nums) :
-            if nums[i] < pivot:
-                lower_bound.append(nums[i])
-            elif nums[i] > pivot:
-                upper_bound.append(nums[i])
-            elif nums[i] == pivot:
-                equal.append(nums[i])
-            i+=1
-        merged_list = lower_bound + equal + upper_bound
+        for num in nums:
+            if num < pivot:
+                lower_bound.append(num)
+            elif num > pivot:
+                upper_bound.append(num)
+            else:
+                equal.append(num)
+        merged_list = lower_bound + equal + upper_bound 
         return merged_list
             
                  
