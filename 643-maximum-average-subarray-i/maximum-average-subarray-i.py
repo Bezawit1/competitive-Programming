ecbee024fcf2570c4fdc560c17f9a752e3066a09
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-10-18 04:28:18 -0700

    Time: 980 ms (25.35%) | Memory: 22.5 MB (98.96%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,12 +1,19 @@
 class Solution(object):
     def findMaxAverage(self, nums, k):
-        	
-        currSum = sum(nums[:k]) 
-        maxSum = currSum
-        for i in range(len(nums)-k):
-            #subtracting the element that rolls out of the window and adding that enters the window
-            currSum -= nums[i]
-            currSum += nums[i+k]
-            maxSum = max(maxSum, currSum)
-        return maxSum/float(k)
-	
+        maxAvg = float('-inf')
+        curr_sum = sum(nums[:k])
+
+        for i in range(len(nums) - k + 1): 
+            curr_avg = curr_sum / float(k)
+            maxAvg = max(maxAvg, curr_avg)
+
+            if i + k < len(nums):
+                curr_sum -= nums[i]
+                curr_sum += nums[i + k]
+
+        return maxAvg
+
+
+
+        
+        
