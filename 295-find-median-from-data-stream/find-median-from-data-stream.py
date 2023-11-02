14ed5363baa9f4f5763bbd04f17ea46b42fc6764
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-11-02 08:55:11 +0300

    Time: 607 ms (79.70%) | Memory: 38.5 MB (54.26%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,19 +1,16 @@
-
-
-class MedianFinder(object):
+class MedianFinder:
 
     def __init__(self):
-        self.min_heap = []  # To store the larger half of numbers
-        self.max_heap = []  # To store the smaller half of numbers
+        self.max_heap = []
+        self.min_heap = []
 
     def addNum(self, num):
         if not self.max_heap or num <= -self.max_heap[0]:
             heapq.heappush(self.max_heap, -num)
         else:
             heapq.heappush(self.min_heap, num)
-        
-        # Balance the heaps if their sizes differ by more than 1
-        if len(self.max_heap) - len(self.min_heap) > 1:
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
             heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
         elif len(self.min_heap) > len(self.max_heap):
             heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
@@ -23,13 +20,3 @@ class MedianFinder(object):
             return (-self.max_heap[0] + self.min_heap[0]) / 2.0
         else:
             return -self.max_heap[0]
-
-
-
-        
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
