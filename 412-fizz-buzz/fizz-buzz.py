8d06f709c0f0ca0e5d8e30cd60757fd2b0bef78e
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-11-10 09:06:57 +0300

    Time: 29 ms (13.19%) | Memory: 14 MB (58.25%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        arr = []
-        for i in range(1,n+1):
-            if i%3 == 0 and i%5==0:
-                arr.append("FizzBuzz")
-            elif i%3 == 0 and i%5!=0:
-                arr.append("Fizz")
-            elif  i%3 != 0 and i%5==0:
-                arr.append("Buzz")
-            else:
-                arr.append(str(i))
-        return arr
+        return [
+            "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else
+            "Fizz" if i % 3 == 0 else
+            "Buzz" if i % 5 == 0 else
+            str(i)
+            for i in range(1, n + 1)
+        ]
 
 
         
