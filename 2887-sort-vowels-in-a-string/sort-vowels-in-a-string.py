77ef05afbd3c0bb84133d9f52d0f4bddf22ba9f6
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-11-13 10:08:20 +0300

    Time: 200 ms (80.82%) | Memory: 20.3 MB (53.42%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def sortVowels(self, s):
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        res = []
+        for i in range(len(s)):
+            if s[i].lower() in vowels:
+                res.append(s[i])
+        res.sort()
+        k = 0
+        for i in range(len(s))  :
+            if s[i].lower() in vowels:
+                s[i] = res[k]
+            k+=1
+        return s
+class Solution(object):
+    def sortVowels(self, s):
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        res = []
+
+        for i in range(len(s)):
+            if s[i].lower() in vowels:
+                res.append(s[i])
+
+        res.sort()
+        k = 0
+        s_list = list(s)
+
+        for i in range(len(s)):
+            if s[i].lower() in vowels:
+                s_list[i] = res[k]
+                k += 1
+
+        return ''.join(s_list)
+
+
+        
+      
+
