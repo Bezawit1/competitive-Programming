0446783a3a100fcf3a2514063d2b394306ed7fb6
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-11-13 10:08:56 +0300

    Time: 193 ms (84.93%) | Memory: 20.2 MB (53.42%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -2,20 +2,6 @@ class Solution(object):
     def sortVowels(self, s):
         vowels = set(['a', 'e', 'i', 'o', 'u'])
         res = []
-        for i in range(len(s)):
-            if s[i].lower() in vowels:
-                res.append(s[i])
-        res.sort()
-        k = 0
-        for i in range(len(s))  :
-            if s[i].lower() in vowels:
-                s[i] = res[k]
-            k+=1
-        return s
-class Solution(object):
-    def sortVowels(self, s):
-        vowels = set(['a', 'e', 'i', 'o', 'u'])
-        res = []
 
         for i in range(len(s)):
             if s[i].lower() in vowels:
