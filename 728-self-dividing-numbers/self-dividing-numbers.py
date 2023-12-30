f5a0476622ae49a20814a88d4ff3029492b9526a
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-12-30 09:08:12 +0300

    Time: 44 ms (27.32%) | Memory: 13.6 MB (19.95%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.py
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -2,18 +2,22 @@ class Solution(object):
     def selfDividingNumbers(self, left, right):
         res = []
         
-        def helper(num):
-            if '0' in str(num):
-                return False
-
-            for digit in str(num):
-                if num % int(digit) != 0:
-                    return False
-
-            return True
-
-        for i in range(left, right + 1):
-            if helper(i):
+        for i in range (left , right+ 1):
+            isDivisible = True
+            n = i
+            while n!=0:
+                digit = n%10
+                if digit == 0:
+                    isDivisible = False
+                    break
+                if i%digit!=0:
+                    isDivisible = False
+                    break 
+                n//=10
+              
+            if(isDivisible):
                 res.append(i)
-
+                print(i)
         return res
+
+        
