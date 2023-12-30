b5f174c802dad43ed44eca32a78d1d5099d89663
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2023-12-30 19:31:19 +0300

    Time: 734 ms (94.35%) | Memory: 28.7 MB (89.19%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -1,15 +1,8 @@
 class Solution(object):
     def divideArray(self, nums, k):
         nums.sort()
-        hashmap = {}
         res = []
        
-        # for i in nums:
-        #     hashmap[i] = hashmap.get(i , 0)+ 1
-     
-        # # for element, frequency in hashmap.items():
-        # #   if frequency > 3:
-        # #     return []
         for i in range(0,len(nums)-2,3):
             diff = nums[i+2] - nums[i]
             
