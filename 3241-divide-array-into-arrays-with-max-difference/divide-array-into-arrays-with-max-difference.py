e7bb53ff043cc18da3441c0ff2602999f88c876e
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-01-04 10:37:32 +0300

    Time: 742 ms (84.56%) | Memory: 28.6 MB (97.86%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -2,19 +2,21 @@ class Solution(object):
     def divideArray(self, nums, k):
         nums.sort()
         res = []
-       
-        for i in range(0,len(nums)-2,3):
+        
+        for i in range (0 , len(nums) - 2, 3):
+            isFound = True
             diff = nums[i+2] - nums[i]
-            
-            if diff > k:
-               return []
+            if diff >k:
+                isFound = False
+                return []
             else:
-               res.append(nums[i:i+3])
-           
+                res.append(nums[i:i+3])
         return res
 
-        
-            
+
+
+
+
 
 
       
