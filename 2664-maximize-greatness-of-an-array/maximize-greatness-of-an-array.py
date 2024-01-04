65a9d479e9cc1124cd8708409676beb069572de7
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-01-04 12:43:18 +0300

    Time: 465 ms (43.24%) | Memory: 24.5 MB (48.65%) - LeetSync

--- 2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.py
+++ 2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.py
@@ -4,11 +4,16 @@ class Solution(object):
         i = 0
         j = 0
         count = 0
+        [1,1,1,2,3,3,5]
         while j < len(nums):
-            if nums[i] <nums[j]:
+            if nums[i] < nums[j]:
                 count+=1
                 i+=1
+            
             j+=1
         return count
-    
+
+        
+
+        
         
