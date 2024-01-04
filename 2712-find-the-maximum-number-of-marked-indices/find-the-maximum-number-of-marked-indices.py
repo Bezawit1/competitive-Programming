cb4bec4cd30662bf2fea525c1aaaa1b5e639b96c
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-01-04 12:52:56 +0300

    Time: 487 ms (100.00%) | Memory: 26 MB (16.67%) - LeetSync

--- 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.py
+++ 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.py
@@ -2,18 +2,18 @@ class Solution(object):
     def maxNumOfMarkedIndices(self, nums):
         nums.sort()
         i = 0
-        j = (len(nums) + 1 )//2
+        j =( len(nums) + 1 )//2
         count = 0
         while j < len(nums):
-            if 2*nums[i] <=nums[j]:
+            if nums[i]*2 <= nums[j]:
                 count+=2
                 i+=1
-                j+=1
-            else:
-                j+=1
+                
+            j+=1
         return count
-         
-        
 
 
+
+ 
+       
         
