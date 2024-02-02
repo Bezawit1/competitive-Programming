93b391832e13d5fe6139cf7bb4d05391e52fdf27
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-02-02 01:46:34 -0800

    Time: 14 ms (76.94%) | Memory: 11.4 MB (99.91%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverse(self, x):
+        sign =1
+        if x <0:
+            sign=-1
+        str_x = str(abs(x))
+        str_x_reversed = "".join(reversed(str_x))
+        num_x_reversed = int(str_x_reversed)
+        if num_x_reversed > 2147483647:
+            return 0
+        return num_x_reversed * sign
+        
+       
