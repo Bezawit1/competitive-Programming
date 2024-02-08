413404a76ad8a3396c8b34c5f7fd8acc6c9ad51e
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-02-08 12:24:46 -0600

    Time: 460 ms (25.16%) | Memory: 27.6 MB (54.38%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,5 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        hashmap={}
-        for i in nums:
-            if i in hashmap:
-                return True
-            hashmap[i] = i
-        return False
-
-        
+        unique = set(nums)
+        return len(nums) != len(unique)
         
