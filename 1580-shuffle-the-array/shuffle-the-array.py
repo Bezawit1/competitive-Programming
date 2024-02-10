c2fd15cbab376bcf820bae647d7c9494d4ef172c
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-02-10 06:53:32 -0600

    Time: 39 ms (45.51%) | Memory: 11.9 MB (73.23%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,12 +1,15 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        i =0
-        j=n
-        arr=[]
-        while i<j and j<len(nums):
-            arr.append(nums[i])
-            arr.append(nums[j])
+        i = 0
+        j = n
+        res = []
+        while i < j and j < len(nums):
+            res.append(nums[i])
+            res.append(nums[j])
             i+=1
             j+=1
-        return arr
+        return res
+
+
+        
         
