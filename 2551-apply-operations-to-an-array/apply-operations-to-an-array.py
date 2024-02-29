3339b3c6e145dcb69c09eece69b9b036a802e841
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-02-29 09:49:36 -0800

    Time: 27 ms (82.69%) | Memory: 12.1 MB (21.15%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,29 +1,17 @@
 class Solution(object):
     def applyOperations(self, nums):
-        i = 0
-        j = len(nums) - 1
-        newA = []
-
-        for i in range(len(nums) - 1):
-            
-            if nums[i] != nums[i + 1]:
-                continue
-            else:
-                nums[i] *= 2
-                nums[i + 1] = 0
-            
-
+        res =[]
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i]*=2
+                nums[i+1]= 0
         for i in nums:
-           if  i!=0:
-               newA.append(i)
-        while len(newA) < len(nums):
-            newA.append(0)
-            
-        return newA
-
-        
-       
+            if i > 0:
+                res.append(i)
+        while len(res) <len(nums):
+            res.append(0)
+        return res
 
 
-        
+
         
