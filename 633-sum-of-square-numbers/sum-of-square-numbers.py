ccc004e68a1a4bc2c73e208d6a2adf77aacc80c6
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-03-07 10:44:56 -0800

    Time: 70 ms (53.67%) | Memory: 11.6 MB (71.75%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,17 +1,16 @@
 class Solution(object):
-   def judgeSquareSum(self , c):
-        i, j = 0, int(c ** 0.5)  
-        while i <= j:
-            current_sum = i * i + j * j
-            if current_sum == c:
-                return True
-            elif current_sum < c:
-                i += 1
-            else:
-                j -= 1
-        return False
-
-
-
-                
+    def judgeSquareSum(self, c):
+            i , j = 0 , int(sqrt(c))
+          
+            while i <=j :
+                product = i*i + j*j
+                if product == c:
+                    return True
+                elif product < c:
+                    i+=1
+                else:
+                    j-=1
+            return False
             
+
+        
