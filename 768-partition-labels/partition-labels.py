19f7bc755c3cbb788877a6c18dfd1d6de9499302
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-03-14 10:46:05 -0700

    Time: 19 ms (64.82%) | Memory: 11.8 MB (17.83%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,20 +1,25 @@
 class Solution(object):
     def partitionLabels(self, s):
-        last_occurrence = {}
-        for i, char in enumerate(s):
-            last_occurrence[char] = i
+        """
+        track the elements and store the index of their last occurence
+        find the maximum size by traversing through the array
+        when the index and last occurence are equal append and strat from the next index until reached the end of the string
+        
+        """
 
-        partitions = []
-        start = 0
-        end = 0
+        occurence = {}
+        res = []
+        start_idx = 0
+        end_idx = 0
+        for i , char in enumerate(s):
+            occurence[char] = i
+        for i , char in enumerate(s):
+            end_idx = max(end_idx , occurence[char])
+            if i == end_idx:
+                res.append(end_idx - start_idx + 1)
+                start_idx = i +1
+        return res
 
-        for i, char in enumerate(s):
-            end = max(end, last_occurrence[char])
-
-            if i == end:
-                partitions.append(end - start + 1)
-                start = i + 1
-
-        return partitions
-
+        
+       
         
