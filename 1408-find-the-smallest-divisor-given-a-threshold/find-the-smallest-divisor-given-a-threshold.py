6c0ee9ce528ac4ad08584af0f2c33fd0a10affd2
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-03-18 09:55:06 -0700

    Time: 293 ms (53.98%) | Memory: 16.4 MB (10.62%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.py
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def smallestDivisor(self, nums, threshold):
-        left , right  =  1, max(nums)
+        i =  1
+        j = max(nums)
        
-        while left <=right:
-            mid=left + (right - left) //2
+        while i <=j:
+            mid=i + (j- i) //2
             total_sum = sum((num + mid - 1) // mid for num in nums)
             if total_sum <=threshold:
-                right = mid -1
+                j= mid -1
             else:
-                left = mid +1
-        return left
+                i = mid +1
+        return i
 
 
         
