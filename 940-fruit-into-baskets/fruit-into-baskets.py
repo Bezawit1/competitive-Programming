25fcbfb03ee86f6f9581c494bf3f653ce5e3975d
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-03-20 08:33:02 -0700

    Time: 700 ms (46.78%) | Memory: 17.4 MB (54.57%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def totalFruit(self,fruits):
             hash_fruits = {}
-            start_index = 0 
-            maxDis =0 
+            idx = 0 
+            max_fruits = 0 
             for i in range(len(fruits)):
                 hash_fruits[fruits[i]] = hash_fruits.get(fruits[i] , 0) + 1
                 while len(hash_fruits) > 2:
-                    hash_fruits[fruits[start_index]] -=1
-                    if hash_fruits[fruits[start_index]] == 0:
-                        del hash_fruits[fruits[start_index]]
-                    start_index+=1
-                maxDis = max(maxDis , i - start_index + 1)
-            return maxDis
+                    hash_fruits[fruits[idx]] -=1
+                    if hash_fruits[fruits[idx]] == 0:
+                        del hash_fruits[fruits[idx]]
+                    idx+=1
+                max_fruits = max(max_fruits , i - idx + 1)
+            return max_fruits
                 
 
 
