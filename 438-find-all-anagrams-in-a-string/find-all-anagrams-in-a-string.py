235e6c551b6019237627e8b0ea740b1b9b7486d2
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-03-20 05:55:36 -0700

    Time: 73 ms (73.88%) | Memory: 12.9 MB (38.51%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,58 +1,29 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        #if length of p is greater than s return empty list
-        if len(p)>len(s):
+        target = {}
+        hash_s = {}
+        res = []
+        window_size = len(p)
+        if len(p) > len(s):
             return []
-        
-        result=[]
-        window_size=len(p)
-        hashmap_p={}
-        hashmap_s={}
-
-        #initialize hashmap p and frequency
-        for char in p:
-            hashmap_p[char]=hashmap_p.get(char,0)+1
-        #initialize the window
+        for i in p:
+            target[i] = target.get(i,0)+1
         for i in range(window_size):
-            char=s[i]
-            hashmap_s[char]=hashmap_s.get(char,0)+1
-        #update the window
-        for i in range(len(s) - window_size + 1):
-            #check if both hashmaps are equal if equal append the index i 
-            if hashmap_p == hashmap_s:
-                result.append(i)
-            #after checking there are valid elements left we assign the left and rightmost index
-            #so that we can track which element leaves the window and 
-            #which will enter
-
+            char = s[i]
+            hash_s[char] = hash_s.get(char, 0) + 1
+        for i in range(len(s)-window_size + 1):
+            if hash_s == target:
+                res.append(i)
+            
             if i + window_size < len(s):
-                left_char=s[i]
-                right_char=s[ i + window_size]
-                
-                hashmap_s[left_char]-=1
-                #if the element frequency reaches 0 then its no longer necessary
-                if hashmap_s[left_char] == 0:
-                    del hashmap_s[left_char]
-                #update the right char since the window is moving 
-                #the right direction
-                hashmap_s[right_char]=hashmap_s.get(right_char,0)+1
-       
-        return result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                char = s[i]
+                right = s[i+window_size]
+                hash_s[char]-=1
+                if hash_s[char] == 0:
+                    del hash_s[char]
+                hash_s[right] = hash_s.get(right,0) +1
+        return res
 
             
+       
         
