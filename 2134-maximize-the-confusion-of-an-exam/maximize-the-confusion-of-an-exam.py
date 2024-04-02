c1c7267ac7a487fbbe3e3809f42b6823780d7202
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-04-02 12:05:32 -0700

    Time: 161 ms (96.30%) | Memory: 14.1 MB (9.26%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -1,27 +1,27 @@
 class Solution(object):
     def maxConsecutiveAnswers(self, answerKey, k):
-            sequence = answerKey
-            length = len(sequence)
+            
+            length = len(answerKey)
             true_count, false_count = 0, 0 
-            max_consecutive_length = 0
-            left = 0
+            max_len = 0
+            j = 0
 
-            for right in range(length):
-                if sequence[right] == 'T':
+            for i in range(length):
+                if answerKey[i] == 'T':
                     true_count += 1
                 else:
                     false_count += 1
 
                 while true_count > k and false_count > k:
-                    if sequence[left] == 'T':
+                    if answerKey[j] == 'T':
                         true_count -= 1
                     else:
                         false_count -= 1
-                    left += 1
+                    j += 1
 
-                max_consecutive_length = max(max_consecutive_length, right - left + 1)
+                max_len = max(max_len, i - j + 1)
 
-            return max_consecutive_length
+            return max_len
 
        
         
