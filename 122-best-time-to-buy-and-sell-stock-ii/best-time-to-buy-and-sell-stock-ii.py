4dd9cb40bdcb4f3d047f527d2b03347cf338ed05
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-04-06 13:15:31 -0700

    Time: 39 ms (50.27%) | Memory: 13 MB (15.75%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def maxProfit(self, prices):
         
+        
         sum_profit = 0
         for i in range(len(prices)-1):
             if prices[i] < prices[i+1]:
                 sum_profit += prices[i+1] - prices[i]
         return sum_profit
 
+        
         
