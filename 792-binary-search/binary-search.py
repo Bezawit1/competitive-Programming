b893d5e4f1f3447f523e06dc3aa00484cd36572a
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-05-09 21:06:05 +0300

    Time: 184 ms (31.21%) | Memory: 12.8 MB (87.87%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,15 +1,17 @@
 class Solution(object):
     def search(self, nums, target):
-        left,right = 0 , len(nums) - 1
-        while left <= right:
-            mid_index = left + (right - left)//2
-            mid_val = nums[mid_index]
-            if mid_val == target:
-                return mid_index
-            elif mid_val < target:
-                left = mid_index + 1
+
+        left = 0
+        right = len(nums) -1
+        while left<=right:
+            mid_element =( left + right)//2
+            if nums[mid_element] == target:
+                return mid_element
+            if nums[mid_element] < target:
+                left = mid_element + 1
             else:
-                right = mid_index - 1
+                right = mid_element - 1
         return -1
 
+        
         
