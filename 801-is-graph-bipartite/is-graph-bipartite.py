2868a8319b19d4108416b6f1a061376a95231530
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-06-20 10:43:00 -0700

    Time: 125 ms (79.63%) | Memory: 12.2 MB (30.00%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def isBipartite(self, graph):
-        # coloring method using dfs 
-#create array of colors initially 0 it represents uncolored nodes
-#create a helper fucntion to traverse each node
+      
         n = len(graph)
         colors = [0] * n 
-        def helper(node, color):
+        def dfs(node, color):
             colors[node] = color
             for i in graph[node]:
-                if colors[i] == color:#neigbor has the same color
+                if colors[i] == color:
                     return False
-                if colors[i] == 0 and not helper(i, -color): # if neigbor is uncolored and not valid results false when called with -color
+                if colors[i] == 0 and not dfs(i, -color):
                     return False
             return True
-#main function 
+ 
         for node in range(n):
-            if colors[node] == 0 and not helper(node, 1):
+            if colors[node] == 0 and not dfs(node, 1):
                 return False
         return True
 
