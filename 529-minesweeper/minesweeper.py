c9839da9a2b258440fef4720b805eb8cea4317c0
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-06-27 12:49:43 -0700

    Time: 130 ms (44.94%) | Memory: 16.1 MB (31.01%) - LeetSync

--- 529-minesweeper/minesweeper.py
+++ 529-minesweeper/minesweeper.py
@@ -1,32 +1,32 @@
 class Solution(object):
     def updateBoard(self, board, click):
-        def count_mines(board, r, c):
-            mines = 0
+        def count_mines(board, row, col):
+            mines_count = 0
             directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
             
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < len(board) and 0 <= nc < len(board[0]) and board[nr][nc] == 'M':
-                    mines += 1
+            for d_row, d_col in directions:
+                new_row, new_col = row + d_row, col + d_col
+                if 0 <= new_row < len(board) and 0 <= new_col < len(board[0]) and board[new_row][new_col] == 'M':
+                    mines_count += 1
                     
-            return mines
-        
-        def dfs(r, c):
-            if not (0 <= r < len(board)) or not (0 <= c < len(board[0])) or board[r][c] != 'E':
+            return mines_count
+
+        def dfs(row, col):
+            if not (0 <= row < len(board)) or not (0 <= col < len(board[0])) or board[row][col] != 'E':
                 return
             
-            mines = count_mines(board, r, c)
+            mines = count_mines(board, row, col)
             if mines == 0:
-                board[r][c] = 'B'
-                for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
-                    dfs(r + dr, c + dc)
+                board[row][col] = 'B'
+                for d_row, d_col in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+                    dfs(row + d_row, col + d_col)
             else:
-                board[r][c] = str(mines)
-        
-        click_r, click_c = click
-        if board[click_r][click_c] == 'M':
-            board[click_r][click_c] = 'X'
+                board[row][col] = str(mines)
+
+        click_row, click_col = click
+        if board[click_row][click_col] == 'M':
+            board[click_row][click_col] = 'X'
         else:
-            dfs(click_r, click_c)
-        
+            dfs(click_row, click_col)
+
         return board
