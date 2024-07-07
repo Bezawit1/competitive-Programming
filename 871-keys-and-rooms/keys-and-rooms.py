d7f3234cc17eff4f43b435190630612bce45d9d2
Bezawit1 <88777464+Bezawit1@users.noreply.github.com>
2024-07-07 05:37:31 -0700

    Time: 68 ms (38.05%) | Memory: 17.1 MB (36.84%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,19 +1,15 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        
-        if not rooms:
-            return False
-        
-        n = len(rooms)
-        visited = [False] * n
+        visited = [False] * len(rooms)
         visited[0] = True
         queue = deque([0])
-        
         while queue:
             current = queue.popleft()
-            for key in rooms[current]:
-                if not visited[key]:
-                    visited[key] = True
-                    queue.append(key)
-        
+            for i in rooms[current]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
         return all(visited)
+        
+    
+
